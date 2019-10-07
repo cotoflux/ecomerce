815529aac54ddd54e88090339a0705824f1932a6
Nuria P <guaupelu@hotmail.com>
2019-10-08 00:08:40 +0200

    First commit base of the index page

---
+++ data.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        "title":"Champú Blanqueante 150ml",
+        "description":"Bienvenido al Chiquito Ipsum, el generador de texto de relleno para tus diseños de antes de los dolores Dale a 'Fistrum' para que te salga ese pedaso de texto Chiquito en estado puro Si te crees muy moderno dale a 'Latin' que te lo pongo con cuarto y mitad de romanooo Jarl!!",
+        "value":22,
+        "discount":5,
+        "img":"https://picsum.photos/200/300",
+        "id":"16277e63-a2ad-4117-881d-2014d86455f2" // Guid
+      },
+      {
+        "title":"Champú Aloe 150ml",
+        "description":"Champú básico",
+        "value":22,
+        "discount":5,
+        "img":"https://picsum.photos/200/300",
+        "id":"16277e63-a2ad-4117-881d-2014d86455f2" // Guid
+      },
+      {
+        "title":"Champú Pelo Negro",
+        "description":"Champú favorece los pelos negros",
+        "value":22,
+        "discount":5,
+        "img":"https://picsum.photos/200/300",
+        "id":"16277e63-a2ad-4117-881d-2014d86455f2" // Guid
+      }
+  ]
---
+++ index.js
@@ -0,0 +1,44 @@
+
+import data from './data.js' 
+
+let app = document.querySelector('#app')
+let container = document.createElement('div')
+// fetch("https://jsonplaceholder.typicode.com/posts").then(res => res.json()).then(data=>console.log(data))
+data.forEach(item=>{
+  
+  let producto = document.createElement('div')
+  let izquierda= document.createElement('div')
+  let derecha = document.createElement('div')
+  
+  let imagen = document.createElement('img')
+  imagen.src=item.img
+  imagen.classList.add('imagen')
+  
+  let ancor = document.createElement('a')
+  ancor.appendChild(imagen)
+  ancor.href="www.google.es";
+  let titulo=document.createElement('p')
+  titulo.innerHTML=item.title
+  let value=document.createElement('p')
+  value.innerHTML=item.value
+  let description=document.createElement('p')
+  description.innerHTML=item.description
+  let submit=document.createElement('button')
+  submit.innerHTML = item.submit
+
+ 
+  izquierda.appendChild(ancor)
+  izquierda.appendChild(titulo)
+  izquierda.appendChild(value)
+
+  derecha.appendChild(description)
+  derecha.appendChild(submit)
+
+  producto.appendChild(izquierda)
+  producto.appendChild(derecha)
+
+  container.appendChild(producto)
+
+})
+
+app.appendChild(container)
