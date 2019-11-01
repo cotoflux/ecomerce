ba35432ac81dff3f50dd7bca12ba9e52385b0a8c
Nuria P <guaupelu@hotmail.com>
2019-11-01 12:25:02 +0100

    Working with functions Codebar.io work 31 Oct

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 
-import data from './data.js' 
+import products from './data.js' 
+
+
+function buildProduct(product){
+  let content=`
+  <div class="layoutProduct">
+    <div>
+      <a href="www.google.es">
+        <img src="${product.img}" class="imagen">
+      </a>
+      <p>${product.name}</p>
+      <p>${product.price}</p>
+     <p>discount = ${product.price  * (product.discount/100)}
+    </div>
+    <div>
+      <p>${product.description}</p>
+    </div>
+  </div>`
+
+  let productDiv = document.createElement('div');
+  productDiv.innerHTML= content;
+  return productDiv;
+
+}
+
+function buildProductsList(){
+
+  products.forEach(item=>{
+    container.appendChild(buildProduct(item));
+    //buildProduct(item);
+  
+  })
+}
 
 let app = document.querySelector('#app')
 let container = document.createElement('div')
+
+
 // fetch("https://jsonplaceholder.typicode.com/posts").then(res => res.json()).then(data=>console.log(data))
-data.forEach(item=>{
-  
-  let producto = document.createElement('div')
-  let izquierda= document.createElement('div')
-  let derecha = document.createElement('div')
-  
-  let imagen = document.createElement('img')
-  imagen.src=item.img
-  imagen.classList.add('imagen')
-  
-  let ancor = document.createElement('a')
-  ancor.appendChild(imagen)
-  ancor.href="www.google.es";
-  let titulo=document.createElement('p')
-  titulo.innerHTML=item.title
-  let value=document.createElement('p')
-  value.innerHTML=item.value
-  let description=document.createElement('p')
-  description.innerHTML=item.description
 
-
- 
-  izquierda.appendChild(ancor)
-  izquierda.appendChild(titulo)
-  izquierda.appendChild(value)
-
-  derecha.appendChild(description)
-
-
-  producto.appendChild(izquierda)
-  producto.appendChild(derecha)
-
-  container.appendChild(producto)
-
-})
 
 app.appendChild(container)
